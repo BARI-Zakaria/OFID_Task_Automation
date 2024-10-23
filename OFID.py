d142732450e551af2d22ccda4f54c9e8b8a62203
Zakaria BARI <zakariabari213@gmail.com>
2024-10-23 17:36:48 +0100

    Updates

---
+++ OFID.py
@@ -0,0 +1,37 @@
+import os
+import glob
+import shutil
+
+# LOOP UNTIL THE USER PROVIDES A VALID PATH
+while True:
+    pathA = input("Please enter the full path of the folder you want to organize: ")
+    
+    # CHECK IF THE PROVIDED PATH EXISTS
+    if os.path.isdir(pathA):
+        break  # If the path exists, exit the loop
+    else:
+        print(f"The path '{pathA}' does not exist. Please try again.\n")
+
+# CONTINUE WITH THE REST OF THE SCRIPT ONCE A VALID PATH IS PROVIDED
+# CREATE AN EMPTY SET AND LIST ALL FILES WITH THE 'GLOB' METHOD
+mySet = set()
+checkList = glob.glob1(pathA, '*.*')
+
+# EXTRACT THE FILE EXTENSION USING 'SPLIT' METHOD AND ADD IT TO 'mySet'
+for files in checkList:
+    mySet.add(os.path.splitext(files)[1])
+
+# CREATE A DIRECTORY WITH THE EXTENSION NAME (excluding the dot '.') INSIDE THE 'pathA' DIRECTORY
+for exts in mySet:
+    direcName = os.path.join(pathA, exts[1:])
+    os.makedirs(direcName, exist_ok=True)  # exist_ok=True ensures that the directory is created if it doesn't already exist
+
+    # MOVE FILES WITH THE CURRENT EXTENSION TO THE CORRECT DIRECTORY
+    for dirpath, dirnames, filenames in os.walk(pathA):
+        for exten in filenames:
+            extension = os.path.splitext(exten)[1]
+            if extension == exts:
+                sourcePath = os.path.join(dirpath, exten)
+                shutil.move(sourcePath, os.path.join(direcName, exten))
+
+print("Files have been successfully organized by extension.")
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Order_Files-Automation
+# OFID Task Automation
 
-Empy README
+OFID (Order Files into Directories) is a task automation tool designed to organize disorganized files in a folder based on their file extension.
+
+Considering the existence of various and different file extensions, this task will be able to analyze the type of each file (for example, .pdf, .docx, .png, etc.) and classify them accordingly. This approach helps maintain a logical and structured order of files within a folder that could otherwise become disorganized, thus facilitating their management and access.
